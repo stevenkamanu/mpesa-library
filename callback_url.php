655d6f3008ae305e4269a11c9daaf1d148448164
wasksofts <stevenkamanu@gmail.com>
2019-04-09 19:13:39 +0300

    Update callback_url.php

--- callback_url.php
+++ callback_url.php
@@ -1,9 +1,7 @@
 <?php
 
-     require_once('../inc/mpesa/Mpesa_model.php');
      require_once('../inc/mpesa/TransactionCallbacks.php');
 
-     $mpesa = new Mpesa_model();
      $callback =  new TransactionCallbacks();
 
 
@@ -12,10 +10,10 @@
        
      //read incoming request
      //$callbackJSONData=file_get_contents('php://input');
-     $result = json_decode($callback->processSTKPushRequestCallback());
+     $result = json_d$callback->processSTKPushRequestCallback());
      //$result =['post'=> $callbackJSONData,'type'=> 'post'];
      
-    if($mpesa->processSTKPushRequestCallback($result)){
+    if($result)){
         $resp = '{"ResultCode":0,"ResultDesc":"Confirmation recieved successfully"}';
     }else{
         $resp = '{"ResultCode":1, "ResultDesc":"Confirmation failure due to internal service error"}';
